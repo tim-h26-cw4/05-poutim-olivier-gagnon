d9fe0a6373a3d540ad20cf836dc5639b4e5390af
Eaulitviez <2077834@cegepmontpetit.ca>
2026-02-24 14:28:39 -0500

    numéro 2 ongoing

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -2,5 +2,13 @@ export default class Chef {
   constructor(element) {
     this.element = element;
     this.container = document.querySelector('.js-container');
+    this.menu = [];
+  }
+  init() {
+    const poutines = document.querySelectorAll('.js-poutine');
+    for (let i = 0; i < poutines.length; i++) {
+      const poutine = poutines[i];
+      this.menu.push(poutine);
+    }
   }
 }
