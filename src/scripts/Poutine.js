b8f55bf908da9d39149ee1f88a024f3a150c9c2e
Eaulitviez <2077834@cegepmontpetit.ca>
2026-02-24 15:05:47 -0500

    bug

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,9 +1,21 @@
 class Poutine {
   constructor(element) {
-    this.init();
     this.element = element;
     this.types = document.querySelectorAll('.button');
     this.selectedType = '';
+    this.init();
   }
-  init() {}
+  init() {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+    }
+  }
+  selectType(event) {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = types[i];
+      type.classList.remove('is-active');
+    }
+    event.currentTarget.classList.add('is-active');
+  }
 }
