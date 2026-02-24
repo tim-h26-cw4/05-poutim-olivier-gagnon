518d3283d4fcfea781e9c05d7e20c0cfd0edd372
Eaulitviez <2077834@cegepmontpetit.ca>
2026-02-24 14:52:16 -0500

    ongoing

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -5,7 +5,9 @@ export default class Chef {
     this.menu = [];
   }
   init() {
+    const btnSubmit = this.element.querySelectorAll('js-submit');
     const poutines = document.querySelectorAll('.js-poutine');
+    btnSubmit.addEventListener('click', this.sendOrder.bind(this));
     for (let i = 0; i < poutines.length; i++) {
       const poutine = poutines[i];
       this.menu.push(poutine);
---
+++ src/scripts/Poutine.js
@@ -0,0 +1,9 @@
+class Poutine {
+  constructor(element) {
+    this.init();
+    this.element = element;
+    this.types = document.querySelectorAll('.button');
+    this.selectedType = '';
+  }
+  init() {}
+}
