87b1ba5a36fd302b869cba3a0f0dd449358aff65
Eaulitviez <2077834@cegepmontpetit.ca>
2026-02-24 14:04:38 -0500

    sauvegarde

---
+++ src/scripts/Chef.js
@@ -0,0 +1,6 @@
+export default class Chef {
+  constructor(element) {
+    this.element = element;
+    this.container = document.querySelector('.js-container');
+  }
+}
--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,20 @@
-import Icons from "./utils/Icons.js";
-
+import Icons from './utils/Icons.js';
+import Chef from './Chef.js';
 // La ligne suivante devrait être au TOUT début du init() du Main
-Icons.load();
+
+class main {
+  constructor() {
+    this.init();
+  }
+  init() {
+    Icons.load();
+    const chefs = document.querySelectorAll("[data-component='Chef']");
+
+    for (let i = 0; i < chefs.length; i++) {
+      const chef = chefs[i];
+      new Chef(chef);
+    }
+  }
+}
+
+new main();
